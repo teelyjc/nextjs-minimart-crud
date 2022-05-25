7e7f4068bd7283bb90b1be27825488f5acf99334
teely44y <ju.tannatee2543@gmail.com>
2022-05-25 13:47:23 +0700

    add: next-connect error handler

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,7 +1,24 @@
 import database from "@middleware/database";
-import nextConnect from "next-connect";
+import { NextApiRequest, NextApiResponse } from "next";
+import nextConnect, { NextHandler } from "next-connect";
 
-const middleware = nextConnect();
+const middleware = nextConnect({
+  onError: (
+    error: NativeError,
+    req: NextApiRequest,
+    res: NextApiResponse,
+    next: NextHandler
+  ) => {
+    console.error(error.stack);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (
+    req: NextApiRequest,
+    res: NextApiResponse,
+  ) => {
+    res.status(404).end("Page not found.")
+  }
+});
 
 middleware
   .use(database)
