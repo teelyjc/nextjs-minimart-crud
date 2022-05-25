f479cc21e519ad5de13e2a006e75682dba839544
teely44y <ju.tannatee2543@gmail.com>
2022-05-25 13:35:08 +0700

    add: check user isAuth ?

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,13 @@
+import useUser from "@hooks/useUser";
+import Router from "next/router";
+import { useEffect } from "react";
+
 export default function Dashboard() {
+  const [user, { mutate }] = useUser();
+
+  useEffect(() => {
+    if (!user) Router.push('/');
+  }, [user])
+
   return <div>Dashboard Page.</div>
 }
